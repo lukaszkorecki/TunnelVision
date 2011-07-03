99af2de329d91360dc11aab4a28c8ca190115528
Łukasz Korecki <lukasz@coffeesounds.com>
2011-07-03 20:24:03 +0100

    options

--- lib/tunnel_vision.rb
+++ lib/tunnel_vision.rb
@@ -1,3 +1,29 @@
 module TunnelVision
+  class Tunnel
+
+    def initialize
+      @pids = []
+    end
+
+    def add command, user, host
+      cmd = 'ssh -L'
+      cmd << user
+      cmd << '@'
+      cmd << host
+
+      cmd << ' '
+      cmd << command
+
+      @pids << spawn(cmd)
+      puts "Spawned #{@pids.last}"
+    end
+
+    def close_all!
+      @pids.each do |pid|
+        puts "Closing #{pid}"
+        `kill -9 #{pid}`
+      end
+    end
+  end
 
 end
--- lib/tunnel_vision/runner.rb
+++ lib/tunnel_vision/runner.rb
@@ -2,8 +2,75 @@ require 'yaml'
 module TunnelVision
   class Runner
     def initialize args
-      fail "Command me! \n#{help()}" if args.empty?
-      y args
+      if args.empty?
+        puts "Command me!"
+        puts help
+        exit 0
+      end
+
+      @tunnel = ::Tunnel.new
+
+      case args.first
+      when 'generate'
+        generate
+
+      when 'start'
+        start
+      when 'stop'
+        stop
+      else
+        puts "Unrecognized command"
+      end
+    end
+
+    def start
+      begin
+        tunnels = YAML::load_file 'tunnels.yaml'
+      rescue
+        puts "No tunnels file!"
+        exit 1
+      end
+
+      tunnels.each do |tunnel_def|
+        @tunnel.add tunnel_def['tunnel'], tunnel_def['user'], tunnel_def['host']
+      end
+    end
+
+    def stop
+      @tunnel.close_all!
+    end
+
+    def help
+      <<-HELP
+        tunnelvision generate - generate example tunnels file
+        tunnelvision start - start all ssh tunnels defined in tunnels.yaml
+        tunnelvision stop - stop all opened ssh tunnels
+      HELP
+    end
+
+    def generate
+      puts "Generating tunnels.yaml"
+      example = {
+        "example" => {
+          "user" => 'bob',
+          'host' => 'example.com',
+          'tunnel' => '123:123.0.0.1:80'
+        },
+          'typical' => {
+          'user' => 'clyde',
+          'host' => 'bar.foo.com',
+          'tunnel' => '7777:bar.foo.com:8080'
+        }
+      }
+
+      begin
+        File.open('tunnels.yaml', 'w') do |file|
+          file.write example.to_yaml
+        end
+
+      rescue "Error when creating tunnels.yaml"
+      end
+      puts "Created!"
     end
   end
 end
